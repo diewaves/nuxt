7f2c0cc997e8454b6a66d58dc04ea0938e396514
Diego <diedb234@gmail.com>
2022-08-18 12:52:32 +0200

    Course Nuxt

---
+++ components/AlbumCard.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <div class="column is-one-quarter">
+        <div class="box">
+            <h3>{{album.title}}</h3>
+            <nuxt-link :to="`/albums/${album.id}`" class="button is-primary">Ver</nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AlbumCard',
+    props:{
+        album: Object
+    }
+}
+</script>
+
+<style lang="css">
+.box{
+    padding: 1em;
+}
+h3{
+    min-height: 4.5em;
+}
+
+</style>
